3a71949d7ef4e28bccf3d6ce4183ec71acab76bd
Yana Arkhipova <frontend.ark@gmail.com>
2022-02-13 21:10:35 +0300

    Add /details/... handler

--- app.rb
+++ app.rb
@@ -71,3 +71,11 @@ post '/new' do
   redirect to '/'
   erb "You typed: #{@content}"    
 end
+
+# вывод информации о посте
+
+get '/details/:post_id' do
+  post_id = params[:post_id]
+
+  erb "Displaing information for post with id #{post_id}"
+end
