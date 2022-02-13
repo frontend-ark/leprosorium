55a79895e68508b4e7f23e337b4d80cdab7ef3fe
Yana Arkhipova <frontend.ark@gmail.com>
2022-02-13 21:45:27 +0300

    Add details view

--- app.rb
+++ app.rb
@@ -75,7 +75,11 @@ end
 # вывод информации о посте
 
 get '/details/:post_id' do
+
   post_id = params[:post_id]
 
-  erb "Displaing information for post with id #{post_id}"
+   results = @db.execute 'SELECT * FROM Posts WHERE Id = ?', [post_id]
+   @row = results[0]
+
+  erb :details
 end
